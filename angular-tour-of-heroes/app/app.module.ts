1fd98d9b74a773e24adf0b906d8eb7cbbd842bf6
Vo Tuan Trung <trung.vo@zyllem.com>
2016-10-19 18:19:05 +0800

    Update content

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router'
+import { HttpModule } from '@angular/http'
+
+import { AppRoutingModule } from './app-routing.module'
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { HeroService } from './hero.service'
 
 import { AppComponent } from './app.component'
 import { HeroesComponent } from './heroes.component'
 import { HeroDetailComponent } from './hero-detail.component'
+import { DashboardComponent } from './dashboard.component'
 
 
 @NgModule({
   imports: [BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
-    ])
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule { }
---
+++ angular-tour-of-heroes/app/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core'
+import { Hero } from './hero'
+import { HeroService } from './hero.service'
+
+import {Router} from '@angular/router'
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-dashboard',
+    styleUrls: ['dashboard.component.css'],
+    templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit {
+    heroes: Array<Hero>;
+    constructor(private heroService: HeroService, private router: Router){}
+
+    ngOnInit():void {
+        this.heroService.getHeroes()
+            .then(response => this.heroes = response);
+    }
+    
+    goToDetail(hero: Hero): void{ 
+        let link = ['detail', hero.id]
+        this.router.navigate(link)
+    }
+}
